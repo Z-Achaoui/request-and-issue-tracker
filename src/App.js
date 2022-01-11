90a10ccb3de9591d9e0fb9322ea5022a7039b5f8
Z-Achaoui <z.achaoui@gmail.com>
2022-01-12 00:10:13 +0100

    added routes and associated pages, navbar, and home and child components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Issues from "./components/Issues";
+import NavBar from "./components/Navbar";
+import NoPage from "./components/NoPage";
+import Requests from "./components/Requests";
+import Support from "./components/Support";
+import logo from "./icons/logo.png";
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex flex-col h-screen">
+      <NavBar logoLink={logo} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/requests" element={<Requests />} />
+        <Route path="/issues" element={<Issues />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </div>
   );
 }
